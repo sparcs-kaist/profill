dcf86f39acdaccf89e979b4addb7ae6394d62e5b
RyuChigon <fbclrhs10@gmail.com>
2022-07-30 23:44:03 +0900

    feat: add axios

---
+++ client/src/utils/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: `${process.env.SERVER_URL}/api`,
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(config => {
+  const token = localStorage.getItem('profill-jwt');
+
+  if (token)
+    config.headers = {
+      'X-Access-Token': `Bearer ${token}`,
+    };
+
+  return config;
+});
+
+export default instance;
