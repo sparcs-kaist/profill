a788327e557f7d85a97027795ea0f4cf00a7564f
Soonho Kwon <percy3368@gmail.com>
2022-07-31 02:07:26 +0900

    handle CORS

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app import api
 from app.core.database import init_db
 
 app = FastAPI()
 
-app.include_router(api.router)
+app.include_router(api.router, prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
